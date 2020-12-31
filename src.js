e24082a192e1f9a1548af4929c9153987491bb0b
rayalva407 <rayalva407@gmail.com>
2020-12-30 22:18:45 -0700

    adds calculate function

---
+++ src.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('#calculate')
+const calculate = () => {
+  const billAmt = document.querySelector('#billAmt').value
+  const percent = document.querySelector('#service').value
+  const people = document.querySelector('#people').value
+
+
+  const tip = (billAmt * percent) / people
+  const resultDiv = document.querySelector('#tip-result')
+  const result = document.createElement('p')
+  result.innerText = `Tip $${tip} each`
+  resultDiv.innerHTML = ''
+  resultDiv.appendChild(result)
+}
+
+button.addEventListener('click', function() {
+  calculate()
+})
