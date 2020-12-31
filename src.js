09bc16f79466fe971744d332677ce0284269e603
rayalva407 <rayalva407@gmail.com>
2020-12-30 23:21:17 -0700

    adds error for inputs

--- src.js
+++ src.js
@@ -4,13 +4,27 @@ const calculate = () => {
   const percent = document.querySelector('#service').value
   const people = document.querySelector('#people').value
 
-  
-  const tip = (billAmt * percent) / people
-  const resultDiv = document.querySelector('#tip-result')
-  const result = document.createElement('h1')
-  people > 1 ? result.innerText = `Tip $${tip} each` : result.innerText = `Tip $${tip}`
-  resultDiv.innerHTML = ''
-  resultDiv.appendChild(result)
+  if (billAmt === "") {
+    document.querySelector('#billAmt').classList.add('errors')
+  }
+  if (percent === "") {
+    document.querySelector('#service').classList.add('errors')
+  }
+  if (people === "") {
+    document.querySelector('#people').classList.add('errors')
+  }
+  if (billAmt !== "" && percent !== "" && people !== "") {
+    document.querySelector('#billAmt').classList.remove('errors')
+    document.querySelector('#service').classList.remove('errors')
+    document.querySelector('#people').classList.remove('errors')
+
+    const tip = (billAmt * percent) / people
+    const resultDiv = document.querySelector('#tip-result')
+    const result = document.createElement('h1')
+    people > 1 ? result.innerText = `Tip $${tip} each` : result.innerText = `Tip $${tip}`
+    resultDiv.innerHTML = ''
+    resultDiv.appendChild(result)
+  }
 }
 
 button.addEventListener('click', function(e) {
